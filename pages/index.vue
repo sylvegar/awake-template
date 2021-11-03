e798f9ad39d39e744efc2aef4586d31d1cecfc07
sylvegar <92884982+sylvegar@users.noreply.github.com>
2021-11-03 17:08:48 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
     <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
+      <!--<button
         v-if="$siteConfig.newsletter.on"
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
       >
         Subscribe To Newsletter
-      </button>
+      </button>-->
     </site-hero>
     <main-section theme="one-column">
       <template v-slot:default>
